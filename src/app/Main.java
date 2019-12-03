e27ad8c04d3b8bfe27c9e7163148e2bd7ef21ab1
Duc Anh <ngducanh1223@gmail.com>
2019-12-03 21:02:03 +0700

    Read path file

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,6 +1,12 @@
 package app;
 
 import app.path.PathGenerator;
+import app.path.PathReader;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,5 +16,27 @@ public class Main {
 
         PathGenerator pathGenerator = new PathGenerator();
         pathGenerator.generate(filePath);
+
+        PathReader pathReader = new PathReader();
+        ArrayList<String> paths = new ArrayList<String>();
+        String pathFile = "/home/ducanh/IdeaProjects/test-generation/CheckTriangle.path";
+        try {
+            File fileContainsPath = new File(pathFile);
+            Scanner myReader = new Scanner(fileContainsPath);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                paths.add(data);
+//                System.out.println(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+        paths.forEach(path -> System.out.println(path));
+
+
+
     }
 }
