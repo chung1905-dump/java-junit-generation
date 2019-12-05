eb60a1d3cb0583bd4c543c2a7ba07e5ff27f1a51
Nguyen Duc Chung <nguyenducchung@sutunam.com>
2019-12-05 14:39:41 +0700

    1F chromosome

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,100 +1,39 @@
 package app;
 
+import app.algorithm.PSO;
 import app.path.Branch;
 import app.path.PathGenerator;
 import app.path.PathReader;
 
-import java.util.ArrayList;
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.ArrayList;
+import java.io.*;
 
 public class Main {
     public static String classUnderTest;
 
     public static void main(String[] args) {
+        String target = "CheckTriangle";
+
         String[] filePath = new String[2];
         filePath[0] = "-d=./out";
-        filePath[1] = "src/app/CheckTriangle.oj";
+        filePath[1] = "src/app/" + target + ".oj";
 
         PathGenerator pathGenerator = new PathGenerator();
         pathGenerator.generate(filePath);
+        editSignatureFile(target + ".sign");
 
         PathReader pathReader = new PathReader();
-        ArrayList<Branch> branches = pathReader.read("./CheckTriangle.path");
-
+        ArrayList<Branch> branches = pathReader.read(target + ".path");
         CheckTriangle.check(1.1, 1.2, 1.3);
-
         java.util.Set set = CheckTriangle.getTrace();
-        System.out.println(set);
+//        System.out.println(set);
 
-        editSignatureFile("./CheckTriangle.sign");
-        readSignature("./CheckTriangle.sign");
-    }
 
-    private static void readSignature(String fileName) {
-        try {
-            Set usedClassNames = new HashSet();
-            String s, r = "";
-            BufferedReader in = new BufferedReader(new FileReader(fileName));
-            while ((s = in.readLine()) != null && !s.equals("#")) {
-                s = s.replaceAll("\\s+", "");
-                if (s.length() > 0) {
-                    String s1 = s.substring(0, s.indexOf("("));
-                    String className = s1.substring(0, s1.lastIndexOf("."));
-                    String methodName = s1.substring(s1.lastIndexOf(".") + 1);
-                    String[] paramNames = s.substring(s.indexOf("(") + 1, s.indexOf(")")).split(",");
-                    if (paramNames.length == 1 && paramNames[0].equals(""))
-                        paramNames = new String[0];
-                    List params = new LinkedList();
-                    for (int i = 0; i < paramNames.length; i++) {
-                        params.add(paramNames[i]);
-                        String usedClass = paramNames[i];
-                        if (paramNames[i].contains("["))
-                            usedClass = paramNames[i].substring(0,
-                                    paramNames[i].indexOf("["));
-                        if (!isPrimitiveType(paramNames[i]))
-                            usedClassNames.add(usedClass);
-                    }
-                    String simpleClassName = className.substring(className.lastIndexOf(".") + 1);
-                    if (simpleClassName.equals(methodName)) {
-//                        MethodSignature methodSign = new MethodSignature(className, params);
-//                        addConstructor(methodSign);
-                    } else {
-//                        MethodSignature methodSign = new MethodSignature(methodName, params);
-//                        addMethod(className, methodSign);
-//                        usedClassNames.add(className);
-                    }
-                    r = s;
-                }
-            }
-            String r1 = r.substring(0, r.indexOf("("));
-            classUnderTest = r1.substring(0, r1.lastIndexOf("."));
-            while ((s = in.readLine()) != null) {
-                if (s.length() > 0) {
-                    String className = s.substring(0, s.indexOf(" as ")).trim();
-                    String typeName = s.substring(s.indexOf(" as ") + 4).trim();
-//                    addConcreteType(typeName, className);
-                }
-            }
-            in.close();
-//            checkConstructorsAvailable(usedClassNames);
-        } catch (IOException e) {
-            System.err.println("IO error: " + fileName);
-            e.printStackTrace();
-            System.exit(1);
-        }
-    }
-
-    private static boolean isPrimitiveType(String type) {
-        if (type.contains("["))
-            type = type.substring(0, type.indexOf("["));
-        return type.equals("int") || type.equals("long") ||
-                type.equals("short") || type.equals("char") ||
-                type.equals("byte") || type.equals("String") ||
-                type.equals("boolean") || type.equals("float") ||
-                type.equals("double");
+        PSO pso = new PSO(target + ".sign");
     }
 
     private static void editSignatureFile(String fileName) {
@@ -104,11 +43,10 @@ public class Main {
 
             FileWriter fileWriter = new FileWriter(file, true);
             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-            PrintWriter printWriter = new PrintWriter(file);
-            printWriter.print("");
-            bufferedWriter.write("java.lang.Integer.Integer(int)\n");
+            bufferedWriter.write("java.lang.Double.Double(double)\n");
+            bufferedWriter.write("app.CheckTriangle.CheckTriangle()\n");
             bufferedWriter.write(content);
-            bufferedWriter.write("#\n");
+            bufferedWriter.write("#");
             bufferedWriter.newLine();
             bufferedWriter.close();
         } catch (IOException e) {
--- src/app/algorithm/PSO.java
+++ src/app/algorithm/PSO.java
@@ -1,13 +1,32 @@
 package app.algorithm;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import it.itc.etoc.ChromosomeFormer;
+import it.itc.etoc.Chromosome;
+import it.itc.etoc.Action;
 
 public class PSO implements AlgorithmInterface {
-    public PSO() {
+    private ChromosomeFormer chromFormer;
+
+    public PSO(String signFile) {
+        initPopulation(signFile);
     }
 
-    private void initPopulation() {
-
+    private void initPopulation(String signFile) {
+        chromFormer = new ChromosomeFormer();
+        chromFormer.readSignatures(signFile);
+        chromFormer.buildNewChromosome();
+        Chromosome chromosome = chromFormer.getChromosome();
+        List actions = chromosome.getActions();
+        System.out.println(chromosome);
+//        for (Object obj : actions) {
+//            Action act = (Action) obj;
+//            System.out.println(1);
+//            System.out.println(act.countPrimitiveTypes());
+//        }
     }
 
     @Override
