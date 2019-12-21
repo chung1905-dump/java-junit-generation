87ae74aae52cc0b406a84af2ea07768836693f73
chung <chung1905@gmail.com>
2019-12-21 16:55:19 +0700

    Fix MethodSign

--- src/app/signature/MethodSignature.java
+++ src/app/signature/MethodSignature.java
@@ -5,27 +5,30 @@ import java.util.List;
 import java.util.Map;
 
 public class MethodSignature extends it.itc.etoc.MethodSignature {
-    private List<Map<String, Object>> paramConditions;
+    private Map<Integer, Map<String, Object>> paramConditions;
 
     public MethodSignature(String s, List list) {
         super(s, list);
     }
 
     public void addParamCondition(int paramNum, String condition, Object value) {
-        Map<String, Object> c = new HashMap<>();
+        Map<String, Object> c = paramConditions.get(paramNum);
+        if (c == null) {
+            c = new HashMap<>();
+        }
         c.put(condition, value);
-        paramConditions.set(paramNum, c);
-    }
-
-    public List<Map<String, Object>> getParamConditions() {
-        return paramConditions;
+        paramConditions.put(paramNum, c);
     }
 
     public Map<String, Object> getParamCondition(int i) {
         return paramConditions.get(i);
     }
 
-    public void setParamConditions(List<Map<String, Object>> paramConditions) {
+    public Map<Integer, Map<String, Object>> getParamConditions() {
+        return paramConditions;
+    }
+
+    public void setParamConditions(Map<Integer, Map<String, Object>> paramConditions) {
         this.paramConditions = paramConditions;
     }
 }
